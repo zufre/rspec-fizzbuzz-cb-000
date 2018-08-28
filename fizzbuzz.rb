3dc0dafd9492cdd24d40573a1ad35353383c4116
Cedric David <cedric.david@btopenworld.com>
2018-08-28 05:58:14 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,14 @@
 
 
 def fizzbuzz(num)
-  if num % 3 === 0
+
+if num % 3 === 0&& num % 5 ===0
+
+  "Fizzbuzz"
+elsif num % 3 === 0
     "Fizz"
   elsif num % 5 ===0
     "Buzz"
-  elsif num % 3 === 0&& num % 5 ===0
   
-    "Fizzbuzz"
   end
 end

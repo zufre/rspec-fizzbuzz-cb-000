21e8996995f1e116dc7ad50c557b460c68839734
Cedric David <cedric.david@btopenworld.com>
2018-08-28 05:52:08 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-require 'pry'
-binding.pry
+
 def fizzbuzz(num)
   if num % 3 === 0
     "Fizz"
